0edef57af34f49ff516bbafbbe5bf98a85a150d0
4015 <4015.dev@pm.me>
2022-08-14 17:31:28 +0100

    add lastChar

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const exists = (x) => x !== undefined && x !== null;
 
 // check if an element exists, an array, and is not empty
 const arrNotEmpty = (arr) => exists(arr) && Array.isArray(arr) && arr.length;
+
+// return the last char of a str
+const lastChar = (str) => str && str.length ? str[str.length-1] : undefined;
